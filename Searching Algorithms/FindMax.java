60f38abc882827724922f28f3950fddfc4b55a71
Jason Lowey <jasonlowey48@gmail.com>
2025-02-12 12:30:33 -0500

    Update FindMax.java

--- Searching Algorithms/FindMax.java
+++ Searching Algorithms/FindMax.java
@@ -16,10 +16,10 @@ public class Main {
     
      int array [] = {5, 29, 99, 3, 48};
       
-      int max = array[0];
+     int max = array[0];
+        
       for(int i = 0; i < array.length; i++){
-          
-      
+
           
           if(array [i] > max){
               max = array[i];
