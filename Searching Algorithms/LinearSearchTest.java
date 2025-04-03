6b8730c50555b9e5b9d11b43093a5d5e6f187295
Jason Lowey <jasonlowey48@gmail.com>
2025-04-03 09:09:37 -0400

    Update LinearSearchTest.java

--- Searching Algorithms/LinearSearchTest.java
+++ Searching Algorithms/LinearSearchTest.java
@@ -1,43 +1,36 @@
 /**
-* This program demonstrates Linear Seach on an array of 8 elements
-* 
-* Last modified: April 3rd, 2025
-*
-* @author Jason Lowey
-* 
-*/
+ * This program demonstrates Linear Seach on an array of 8 elements
+ *
+ * Last modified: April 3rd, 2025
+ *
+ * @author Jason Lowey
+ *
+ */
 
 class LinearSearchTest {
+  public static int linearSearch(int[] array, int target) {
+    boolean found;
 
-     public static int linearSearch(int [] array, int target){
-        
-        boolean found;
-        
-        for(int i = 0; i < array.length; i++){
-            
-            if(target == array[i]){
-               return i;
-                
-            }
-        }
-        
-        return -1;     
+    for (int i = 0; i < array.length; i++) {
+      if (target == array[i]) {
+        return i;
+      }
     }
 
+    return -1;
+  }
 
-    public static void main(String[] args) {
-        
-         int [] array = {48,19,13,72,9,51,47,3};
-         
-         int index = linearSearch(array,72);
-         
-         if(index == -1){
-             
-             System.out.println("Target not found");
-         }
-         
-         else{
-             System.out.println("target found at index: " + index);
-         }
+  public static void main(String[] args) {
+    int[] array = {48, 19, 13, 72, 9, 51, 47, 3};
+
+    int index = linearSearch(array, 72);
+
+    if (index == -1) {
+      System.out.println("Target not found");
     }
+
+    else {
+      System.out.println("target found at index: " + index);
+    }
+  }
 }
